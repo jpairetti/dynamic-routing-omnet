c6ca8eacaffe9dfacf33ac03091cc251f7e4cd0e
Lissandro Bosque <lissandro.bosque@mi.unc.edu.ar>
2026-06-09 02:47:06 +0000

    feat: net y packet

--- Net.cc
+++ Net.cc
@@ -2,6 +2,7 @@
 #define NET
 
 #include <string.h>
+#include <map>
 #include <omnetpp.h>
 #include <packet_m.h>
 
@@ -9,7 +10,21 @@ using namespace omnetpp;
 
 class Net: public cSimpleModule {
 private:
+    // ruta aprendida del mejor camino para llegar a un destino dado
+    struct Route {
+        int outGate;   // toLnk[], por que interfaz salir (0,1) en este caso
+        int hopCount;  // saltos que hay que dar para llegar
+    };
 
+    std::map<int, Route> routingTable;  // dict que relaciona numeros(nodos) con Route's
+    cMessage *helloTimer;
+    simtime_t helloInterval;
+    bool UseShortestPath; // false -> usar idea del kick, true -> usar nuestro algo
+    int myId; // simplificacion del codigo
+    int numIfaces; // punto *
+
+    void broadcastHello();
+    void learnRoute(int destId, int viaGate, int hops);
 public:
     Net();
     virtual ~Net();
@@ -24,31 +39,110 @@ Define_Module(Net);
 #endif /* NET */
 
 Net::Net() {
+    helloTimer = NULL;
 }
 
 Net::~Net() {
+    cancelAndDelete(helloTimer);
 }
 
 void Net::initialize() {
+    myId = getParentModule()->getIndex();
+    numIfaces = gateSize("toLnk$o");
+    helloInterval = par("helloInterval");
+    UseShortestPath = par("useShortestPath");
+
+    // Descubrimiento de topología: cada nodo inunda la red con paquetes HELLO 
+    // por todas sus interfaces. A medida que viajan, cada nodo aprende qué tan
+    // lejos está cada destino y por qué interfaz. Se reenvían periódicamente
+    // para mantener la tabla actualizada.
+    // El kickstarter no usa esto, por lo que no genera tráfico de control.
+    if (UseShortestPath) {
+        helloTimer = new cMessage("helloTimer");
+        scheduleAt(simTime() + uniform(0, 1), helloTimer);
+    }
 }
 
 void Net::finish() {
 }
 
+void Net::broadcastHello() {
+    for (int g = 0; g < numIfaces; g++) {
+        Packet *hello = new Packet("hello");
+        // getters y setters de packet definidos en packet_m.cc
+        hello->setIsHello(true);
+        hello->setByteLength(64);     // paquete de control => chico
+        hello->setSource(myId);       // Asigno id del nodo al paquete
+        hello->setDestination(-1);    // unused for control traffic
+        hello->setHopCount(1);        // distancia recorrida, se acaba de crear
+                                      // cuando llegue al vecino, recorrio 1  
+        send(hello, "toLnk$o", g);
+    }
+}
+
+void Net::learnRoute(int destId, int viaGate, int hops) {
+    auto it = routingTable.find(destId);
+    // keep the shortest of the (at most two, in a ring) paths we hear about
+    if (it == routingTable.end() || hops < it->second.hopCount) {
+        routingTable[destId] = Route{viaGate, hops};
+    }
+}
+
 void Net::handleMessage(cMessage *msg) {
 
-    // All msg (events) on net are packets
+    // MENSAJE INTERNO: enviar paquetes hello
+    if (msg == helloTimer) {
+        broadcastHello();
+        scheduleAt(simTime() + helloInterval, helloTimer);
+        return;
+    }
+
     Packet *pkt = (Packet *) msg;
 
-    // If this node is the final destination, send to App
-    if (pkt->getDestination() == this->getParentModule()->getIndex()) {
+    // MENSAJE EXTERNO: llega paquete hello
+    if (pkt->isHello()) {
+        int arrivalGate = pkt->getArrivalGate()->getIndex();
+        int originId = pkt->getSource();
+        int hops = pkt->getHopCount();
+
+        // veo si el paquete es el que yo habia enviado y ya regreso
+        if (originId == myId) {
+            delete msg;
+            return;
+        }
+
+        // contiene la logica de decidir y reaccionar en base a si el paquete 
+        // hello tiene mejor ruta a un destino conocido o si nos esta 
+        // informando de una ruta desconocida para nos
+        learnRoute(originId, arrivalGate, hops);
+        
+        // incremento los pasos del paquete y envio por la misma direccion en 
+        // la que venia
+        pkt->setHopCount(hops + 1);
+        send(pkt, "toLnk$o", 1 - arrivalGate);
+        return;
+    }
+
+    // ---- regular data packet -------------------------------------------
+
+    // si soy el destino final, envio el msj a la capa de aplicacion
+    if (pkt->getDestination() == myId) {
         send(msg, "toApp$o");
+        return;
     }
-    // If not, forward the packet to some else... to who?
-    else {
-        // We send to link interface #0, which is the
-        // one connected to the clockwise side of the ring
-        // Is this the best choice? are there others?
-        send(msg, "toLnk$o", 0);
+
+    // Por defecto puerta 0 (kickstarter). Si el enrutamiento adaptativo
+    // está activo y existe una ruta aprendida, usar el camino más corto.
+    pkt->setHopCount(pkt->getHopCount() + 1);
+
+    int outGate = 0; 
+
+    if (UseShortestPath) {
+
+        auto it = routingTable.find(pkt->getDestination());
+        if (it != routingTable.end())
+            outGate = it->second.outGate;
     }
+
+    send(msg, "toLnk$o", outGate);
 }
